90c3fa1b2db9671b7336574ad6ee520bfd766497
Samantha Orizabal <sam.orizabal@gmail.com>
2024-02-28 18:23:46 -0500

    NeighbourDaoImpl roughly done

--- app/dao/NeighbourDaoImpl.py
+++ app/dao/NeighbourDaoImpl.py
@@ -1,5 +1,9 @@
+from dao.db import TransactionHandler
 from .NeighbourDao import NeighbourDao
 
 class NeighbourDaoImpl(NeighbourDao):
     def get_all_neighbours(self, id):
-        pass
+        with TransactionHandler() as cursor:
+            query = f"SELECT * FROM neighbours WHERE module_id = {id};"
+            cursor.execute(query)
+            return cursor.fetchall()
