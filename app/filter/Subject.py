71c871da907a208840e10d15233aff314b30e80f
Samantha Orizabal <sam.orizabal@gmail.com>
2024-04-11 18:41:33 -0400

    Reformat a bit

--- app/filter/Subject.py
+++ app/filter/Subject.py
@@ -17,11 +17,7 @@ class ModuleSubject(Subject):
         super().__init__()
 
     def on_next(self, audioItems: List[AudioItem]):
-        pool = concurrent.futures.ThreadPoolExecutor(max_workers=3)
-
         def processAudio(a: AudioItem):
-            # print(f"Thread {threading.current_thread().ident} filtering audio")
-            # audio = AudioSegment.from_file(f'./audio_data/{a.ref}.wav', format='wav')
             audioPath = f'./audio_data/{a.ref}.wav'
             _, data = wavfile.read(audioPath)
             # Filter data
@@ -30,15 +26,13 @@ class ModuleSubject(Subject):
             # update audioItem with audio
             a.ref = audioPath
             self.audioItemDao.update(a)
-        
-        def threadedProcess():
-            for a in audioItems:
-                pool.submit(processAudio(a))
 
-            # Wait for threads to finish
-            pool.shutdown(wait=True)
+        with concurrent.futures.ThreadPoolExecutor(max_workers=3) as executor:
+            def threaded():
+                executor.map(processAudio, audioItems)
+                executor.shutdown(wait=True)
 
-        self.metrics.trackExecutionTime(threadedProcess)
+            self.metrics.trackExecutionTime(threaded)
         
         # Re-emit event to be consumed by triangulation
         super().on_next(audioItems)
